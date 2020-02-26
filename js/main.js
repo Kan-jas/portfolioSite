17920f98bc2cb42f3194a2e8de0091bc1abad0fc
kanjas <kajio0024@gmail.com>
2020-02-26 23:01:39 +0900

    作品部分を完成。カルーセルパネルの実装。

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
 $(function() {
 
+  const carouselWidth = $(".works-part").width(); //560
+  const length = $("#carousel-inner li").length; // 4
+  const innerWidth = carouselWidth * length; // 2240
+  const time = 400; // transition = 0.4秒
+
+  const carouselinner = $("#carousel-inner");//ul
+  const prev = $("#prev");
+  const next = $("#next");
+
+  let c = 1;//現在表示されてる画像を表示させるもの
+
   // ハンバーガーメニュー
   $('#humburger').click(function() {
     $(this).toggleClass("active");
@@ -7,7 +18,47 @@ $(function() {
     $('#main').toggleClass("hide");
   });
 
+
+// 次へのボタン処理
+    $('#carousel-nav img').eq(0).css("opacity",0.5);
+next.click(function(){
+
+  if(c == length){
+    carouselinner.stop().animate({
+      left:"0"},time);
+    c=1;
+    $('#carousel-nav img').css("opacity",1);
+    $('#carousel-nav img').eq(c-1).css("opacity",0.5);
+  } else {
+    carouselinner.stop().animate({
+      left: -c * carouselWidth
+    },time);
+    c++;
+    $('#carousel-nav img').css("opacity",1);
+    $('#carousel-nav img').eq(c-1).css("opacity",0.5);
+  }
 });
+
+// 前へのボタン処理
+prev.click(function(){
+
+  if(c==1){
+    carouselinner.stop().animate({
+      left:"-1680px"
+    },time);
+    c=length
+    $('#carousel-nav img').css("opacity",1);
+    $('#carousel-nav img').eq(c-1).css("opacity",0.5);
+  }else{
+    carouselinner.stop().animate({
+      left:(-c+2)*carouselWidth
+    },time);
+    c--;
+    $('#carousel-nav img').css("opacity",1);
+    $('#carousel-nav img').eq(c-1).css("opacity",0.5);
+  }
+});
+
 // $(window).scroll(function() {
 
   //   let value = $(this).scrollLeft();
@@ -28,3 +79,5 @@ $(function() {
 //       return false;
 //   });
 // });
+
+});
